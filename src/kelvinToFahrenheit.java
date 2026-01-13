91f8259f1192ab467d2cda17ed74687c9d8437ff
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-01-13 14:19:52 +0530

    added kelvin to fahrenheit conversions

--- src/kelvinToFahrenheit.java
+++ src/kelvinToFahrenheit.java
@@ -1,2 +1,12 @@
+import java.util.Scanner;
 public class kelvinToFahrenheit {
+    static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        for (int i=0;i<5;i++){
+            System.out.println("Enter the temperature");
+            int kelvin = input.nextInt();
+            double value =(kelvin-273.15)*1.8;
+            System.out.println("the temperture is ="+(value+32));
+        }
+    }
 }
