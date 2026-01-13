70166386e432077063a0cc8f3c5c92ff11a47216
Dheerajparihar111 <parihardheeraj.p@gmail.com>
2026-01-13 13:24:31 +0530

    added kelvin to degree celsius conversions

--- src/celsiusToKelvin.java
+++ src/celsiusToKelvin.java
@@ -4,7 +4,7 @@ public class celsiusToKelvin {
    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         for (int i =0;i<5;i++) {
-            System.out.println("enter the temperature");
+            System.out.println("enter the tempera ture");
             int celsius = input.nextInt();
             float value = 273.15f;
             float kelvin = celsius + value;
